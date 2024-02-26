6022642e530d79f39292254d4174169a92511f34
sorin vieriu <sorinvieriu@gmail.com>
2024-02-26 16:31:37 +0200

    fix: updates

---
+++ index-ws.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const server = require("http").createServer();
+
+const app = express();
+
+app.get("/", (req, res) => {
+	res.sendFile("index.html", {
+		root: __dirname,
+	});
+});
+
+server.on("request", app);
+server.listen(3000, function () {
+	console.log("Server running on port 3000!");
+});
